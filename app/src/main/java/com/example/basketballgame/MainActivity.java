23f2c45a27ac81f03faf670b6265ee3007fe00d2
Tanushree Shaw <63643049+TanushreeShaw@users.noreply.github.com>
2020-12-23 20:49:56 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/basketballgame/MainActivity.java
+++ app/src/main/java/com/example/basketballgame/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.basketballgame;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
